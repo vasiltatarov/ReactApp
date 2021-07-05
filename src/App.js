1de889762c66b5d75b38ff225653c625c70d9d7f
vasiltatarov <vasiltatarov3@gmail.com>
2021-07-05 19:35:27 +0300

    made components for books. fetch data from the server and render it in the browser

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
-async function getData() {
-  const response = await fetch('https://localhost:44318/WeatherForecast');
-  const data = await response.text();
-  console.log(data);
-}
-
-getData();
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route to='/' exact component={Home} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import Books from '../Books';
+
+function Home() {
+    return (
+        <>
+            <Books />
+        </>
+    );
+}
+
+export default Home;
