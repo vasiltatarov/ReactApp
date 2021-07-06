47de516993d4fb48f5bf06103cfcaed202e0d5b0
vasiltatarov <vasiltatarov3@gmail.com>
2021-07-06 11:38:12 +0300

    add bootstrap and use card from it

--- src/components/Book/Books.js
+++ src/components/Book/Books.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import BookCard from './BookCard';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+
 import './Books.css';
 
 class Books extends Component {
@@ -20,21 +27,31 @@ class Books extends Component {
 
     render() {
         return (
-            <div className='cards'>
-                <h1>Check out these EPIC Destinations!</h1>
-                <div className='cards__container'>
-                    <div className='cards__wrapper'>
-                        <ul className='cards__items'>
-                            {this.state.books.map(x =>
-                                <BookCard
-                                    key={x.id}
-                                    name={x.name}
-                                    author={x.author}
-                                />)}
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <Container>
+                <Row md={4}>
+                    {this.state.books.map(x =>
+                        <Card key={x.id} style={{ width: '18rem', margin: '10px', padding: '10px' }}>
+                            <Card.Img variant="top" src="https://img.jakpost.net/c/2019/03/02/2019_03_02_66706_1551461528._large.jpg" />
+                            <Card.Body>
+                                <Card.Title>{x.name}</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of
+                                    the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                            <ListGroup className="list-group-flush">
+                                <ListGroupItem>Author: {x.author}</ListGroupItem>
+                                <ListGroupItem>Year: {x.year}</ListGroupItem>
+                                <ListGroupItem>IMDB Rating: {x.imdb}</ListGroupItem>
+                            </ListGroup>
+                            <Card.Body>
+                                <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    )}
+                </Row>
+            </Container>
         );
     }
 }
