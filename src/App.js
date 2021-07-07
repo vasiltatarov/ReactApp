1a349cebb1393b4233c043e18839b2e5f5369ed6
vasiltatarov <vasiltatarov3@gmail.com>
2021-07-07 17:15:52 +0300

    add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './components/pages/Home';
 import Book from './components/pages/Book';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/books' exact component={Book} />
         </Switch>
+        <Footer />
       </Router>
     </>
   );
